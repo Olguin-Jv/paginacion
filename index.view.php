e04c7dea2954b942eaf78710d805b79e2470cf18
OlguinJv <reminiscent.nightlight@gmail.com>
2018-05-28 22:15:59 -0300

    first commit

---
+++ index.view.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="estilos.css">
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <title>Paginación</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1>Artículos</h1>
+        <section class="articulos">
+            <ul>
+                <li>1. - Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                <li>2. - Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                <li>3. - Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                <li>4. - Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                <li>5. - Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+            </ul>
+        </section>
+        <section class="paginacion">
+            <ul>
+                <li class="disabled"><a href="#">&laquo;</a></li>
+                <li class="active"><a href="#">1</a></li>
+                <li><a href="#">1</a></li>
+                <li><a href="#">1</a></li>
+                <li><a href="#">1</a></li>
+                <li><a href="#">&raquo;</a></li>
+            </ul>
+        </section>
+    </div>
+</body>
+</html>
