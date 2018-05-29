706cffa53f68896086be8242917948969258ad79
OlguinJv <reminiscent.nightlight@gmail.com>
2018-05-29 00:34:48 -0300

    end project -paginacion en php-

--- index.view.php
+++ index.view.php
@@ -13,21 +13,38 @@
         <h1>Artículos</h1>
         <section class="articulos">
             <ul>
-                <li>1. - Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-                <li>2. - Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-                <li>3. - Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-                <li>4. - Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-                <li>5. - Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+              <?php foreach ($articulos as $articulo ): ?>
+                <li><?php echo $articulo['id'] . '.-' . $articulo['articulo'] ?></li>
+              <?php endforeach; ?>
             </ul>
         </section>
         <section class="paginacion">
             <ul>
-                <li class="disabled"><a href="#">&laquo;</a></li>
-                <li class="active"><a href="#">1</a></li>
-                <li><a href="#">1</a></li>
-                <li><a href="#">1</a></li>
-                <li><a href="#">1</a></li>
-                <li><a href="#">&raquo;</a></li>
+              <!-- Establecemos cuando el boton de "Anterior" estará desabilitado  -->
+              <?php if ($pagina == 1): ?>
+                <li class="disabled">&laquo;</li>
+              <?php else: ?>
+                <li><a href="?pagina=<?php echo $pagina - 1 ?>">&laquo;</a></li>
+              <?php endif; ?>
+
+              <!-- Ejecutamos el ciclo para mostrar las páginas -->
+              <?php
+              for ($i=1; $i <= $numeroPaginas ; $i++) {
+                if ($pagina == $i) {
+                  echo "<li class='active'> <a href='?pagina=$i'>$i</a> </li>";
+                } else {
+                  echo "<li><a href='?pagina=$i'>$i</a></li>";
+                };
+              };
+               ?>
+               
+               <!-- Establecemos cuando el botón de "Siguiente" estará desabilitado -->
+               <?php if ($pagina == $numeroPaginas): ?>
+                 <li class="disabled">&laquo;</li>
+               <?php else: ?>
+                 <li><a href="?pagina=<?php echo $pagina + 1 ?>">&raquo;</a></li>
+               <?php endif; ?>
+
             </ul>
         </section>
     </div>
